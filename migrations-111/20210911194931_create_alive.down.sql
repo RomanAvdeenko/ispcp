6f4a7779a39b1edb9b818b5b2d8a93f5ad4e955e
rav <rav@r1.hl.ua>
2021-10-16 01:29:57 +0300

    init

--- migrations-111/20210911194931_create_alive.down.sql
+++
@@ -1,1 +0,0 @@
-DROP TABLE `alive_dev`;
---
+++ migrations/20211015222500_alive_dev.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE  IF NOT EXISTS `alive_dev` (
+ `alive_ip` int(10) unsigned NOT NULL,
+ `alive_is` tinyint(1) NOT NULL,
+ `alive_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ `alive_mac` char(20) NOT NULL DEFAULT '0',
+ PRIMARY KEY (`alive_ip`) USING BTREE,
+ KEY `alive_ts` (`alive_ts`) USING BTREE,
+ KEY `alive_is_ip` (`alive_is`,`alive_ip`) USING BTREE
+) ENGINE=MEMORY DEFAULT CHARSET=utf8
