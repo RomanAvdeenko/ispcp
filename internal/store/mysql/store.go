fda3d30f7acf7265bff98831038149dcf4b87765
RomanAvdeenko <yes@hl.ua>
2021-11-09 22:11:58 +0200

    Refactor

--- internal/store/mysql/store.go
+++ internal/store/mysql/store.go
@@ -3,7 +3,6 @@ package mysql
 import (
 	"database/sql"
 	"ispcp/internal/model"
-	"log"
 
 	mynet "github.com/RomanAvdeenko/utils/net"
 )
@@ -21,7 +20,6 @@ func (s *Store) Store(pongs *model.Pongs) error {
 
 	ps := pongs.LoadAll()
 	for _, v := range *ps {
-		log.Default().Println("Write to db: " + v.Human())
 		_, err := s.db.Exec(INSERT_QUERY, mynet.Ip2int(v.IpAddr), v.Alive, v.Time, v.MACAddr.String())
 		if err != nil {
 			return err
